08f962291de2d2b105b34a67fba25984905a19bf
723poil <leetkdguq73@naver.com>
2025-07-21 21:39:08 +0900

    apply nestia

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { TypedRoute } from "@nestia/core";
+import { Controller } from "@nestjs/common";
+import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  /**
+   * Get
+   * @returns
+   */
+  @TypedRoute.Get()
   getHello(): string {
     return this.appService.getHello();
   }
